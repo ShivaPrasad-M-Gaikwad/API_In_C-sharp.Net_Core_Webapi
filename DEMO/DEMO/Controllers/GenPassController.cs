6c207875699b81acc7895973c845aac6d3bc4397
COMMANDO <88733524+Dev-ShivaPrasad@users.noreply.github.com>
2024-04-18 22:12:23 +0530

    Initial commit

---
+++ DEMO/DEMO/Controllers/GenPassController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace DEMO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenPassController : ControllerBase
+    {
+
+        [HttpGet]
+        public string getpass(int size)
+        {
+            return Gen(10);
+        }
+
+        public string Gen(int size)
+        {
+            string StoredPassword;
+            char Generated_Password;
+
+            StringBuilder SB = new StringBuilder();
+            Random Rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                Generated_Password = (char)Rand.Next(33, 126);
+                SB.Append(Generated_Password);
+            }
+            StoredPassword = SB.ToString();
+            return StoredPassword;
+        }
+    }
+}
